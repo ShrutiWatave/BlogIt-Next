cb45dc8fba48db63d728c4cb6341cfbbbb6036ed
Shruti Watave <shrutiwatave21@gmail.com>
2024-01-10 18:14:36 +0530

    Featured component

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import styles from './featured.module.css'
+import Image from 'next/image'
 
 const Featured = () => {
   return (
-    <div>
-      Featured
+    <div className={styles.container}>
+      <div className={styles.title}>
+      <h1>Hey, this is the heading! In minima dolorum natus. Incidunt fugia</h1>
+      </div>
+      <div className={styles.post}>
+
+      <div className={styles.imgContainer}>
+        <Image className={styles.image} src="/p1.jpeg" alt="p1.jpeg" fill/>
+      </div>
+      <div className={styles.textContainer}>
+
+        <h1 className={styles.postTitle}>Lorem, ipsum dolor sit amet consectetur </h1>
+        <p className={styles.postDescription}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, vitae? Culpa officiis repellendus nihil velit fuga magnam nemo? Voluptate illum ipsum modi nemo fugit necessitatibus, iure dignissimos excepturi dolores velit?
+        </p>
+
+        <button className={styles.button}>Read more</button>
+      </div>
+      </div>
+
     </div>
   )
 }
